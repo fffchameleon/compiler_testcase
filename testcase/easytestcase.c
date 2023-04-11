c07c7f173d64e80f9ddfe8842f6d3e189fe42fa7
aaaallleen <67535530+aaaallleen@users.noreply.github.com>
2023-04-11 20:10:53 +0800

    Add files via upload
    EASY TESTCASE! SHOULD BE EASY TO PASS

---
+++ testcase/easytestcase.c
@@ -0,0 +1,37 @@
+int main(int argc, char *argv[])
+for(int i = 0 ; i <= 10; i++)
+/*this is a multi line comment
+this is the second line of the comment*/
+      #pragma           source off
+//another comment
+long long int functioncall(int a, const int b, double *c)
+struct data{
+  public:
+    data (int x = 1): x(x) {} = default;
+    int getcardnum();
+    void printname();
+  private:
+    int cardnum;
+    char* familyname;
+  
+}
+void swap(int &a, int &b){
+  int tmp = a;
+  a = b;
+  b = tmp;
+  return;
+}
+int negate(int a){
+  return !a;
+}
+- 0.23
+-0.23
+char arr[]
+int a = 3%2
+bitwise operator a > 3&2;
+_ValidID
+V___
+32abc
+_
+__counter__
+__COUNTER__
